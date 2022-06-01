f64f9f5e82730265c67f4edf27d4a20070d72078
ezraa1 <ezrakipchirchir1034@gmail.com>
2022-06-01 16:57:46 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,49 @@
+function add(a,b) {
+    return a+b;
+}
+
+1 + 80; 
+function subtract(a,b){
+    return a-b;
+}
+
+60 - 40; 
+function multiply(a,b){
+    return a*b;
+}
+2 * 3.4; 
+function divide(a,b){
+    return a/b;
+}
+5.0 / 2.5; 
+
+let n=983;
+function increment(n) {
+	return n +=1;
+}
+let d=31;
+function decrement(d) {
+	return  d-=1;
+}
+// console.log()
+parseInt("2.222",10);
+console.log(parseInt("2.222",10))
+
+parseInt("0",10);
+console.log(parseInt("0", 10))
+
+parseFloat("2.222" ,10);
+console.log(parseFloat("2.222", 10))
+
+parseInt("2",10);
+console.log(parseInt("2", 10))
+function makeInt(n){
+	return parseInt(n, 10)
+}
+ console.log(parseInt(n, 10))
+
+
+function preserveDecimal(n){
+    return parseFloat(n)
+}
+console.log(parseFloat(n))
